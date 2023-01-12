1c96cea1610a1fa232cf6e3cac62d453d2509aaf
Guillermo Mayoral <espart-187@outlook.com>
2023-01-11 20:58:48 -0600

    preentrega 3 finalizada

--- js/scripts.js
+++ js/scripts.js
@@ -35,19 +35,19 @@ const tenisNike = {
 const tenisAdidas = {
     id:"2",
     marca:"adidas",
-    costo:"$2,499.00",
+    costo:"$2,099.00",
     img:"../img/productos/producto2.png.png",
 }
 
 const tenisSketchers = {
     id:"3",
     marca:"sketchers",
-    costo:"$2,499.00",
+    costo:"$2,299.00",
     img:"../img/productos/producto3.png.png",
 }
 
 const carritoCompras = []
-console.log(carritoCompras);
+const carritoComprasDatos = []
 
 //selectores de productos
 let btnNike = document.getElementById('btnNike')
@@ -59,35 +59,115 @@ let btnSketchers = document.getElementById('btnSketchers')
 let cuadroCarrito = document.getElementById('carroDeComprasTenis')
 
 let varCarrito = 0;
+let varCarritoDatos = 0;
 
 console.log(varCarrito);
 
 if(btnNike){
     btnNike.addEventListener('click', ()=>{
-        cuadroCarrito.innerHTML += '<div id="'+varCarrito+'" class="cardShop"><img src="../img/productos/producto1.png.png" alt="tenis nike"><div><p>Modelo: Nike</p><p>costo: $2,499.00</p></div></div>'
-        carritoCompras[varCarrito]={
-            id:"1",
-            marca:"nike",
-            costo:"$2,499.00",
-            img:"../img/productos/producto1.png.png",
-        };
+        cuadroCarrito.innerHTML += '<div id="'+varCarrito+'" class="cardShop"><img src="'+tenisNike.img+'" alt="tenis nike"><div><p>Modelo: '+tenisNike.marca+'</p><p>costo: '+tenisNike.costo+'</p></div></div>'
+        carritoCompras[varCarrito]=2499;
         varCarrito = varCarrito+1;
+        carritoComprasDatos.push("Tenis: nike.  $2499.00")
     })
 }
 
-//borrar productos
+if(btnAdidas){
+    btnAdidas.addEventListener('click', ()=>{
+        cuadroCarrito.innerHTML += '<div id="tenisAdidas" class="cardShop"><img src="'+tenisAdidas.img+'" alt="tenis adidas"><div><p>Modelo: '+tenisAdidas.marca+'</p><p>costo: '+tenisAdidas.costo+'</p></div></div>'
+        carritoCompras[varCarrito]=2499;
+        varCarrito = varCarrito+1;
+        carritoComprasDatos.push("Tenis: adidas.  $2099.00")
+    })
+}
+
+
+if(btnSketchers){
+    btnSketchers.addEventListener('click', ()=>{
+        cuadroCarrito.innerHTML += '<div id="tenisSketchers" class="cardShop"><img src="'+tenisSketchers.img+'" alt="tenis sketchers"><div><p>Modelo: '+tenisSketchers.marca+'</p><p>costo: '+tenisSketchers.costo+'</p></div></div><div></div>'
+        carritoCompras[varCarrito]=2499;
+        carritoComprasDatos.push("Tenis: sketchers.  $2299.00")
+        
+    })
+}
+
+
+//valores form productos
+let userProduct = document.getElementById('nombreForm');
+let lastNameProduct = document.getElementById('apellidoForm');
+let cellProduct = document.getElementById('numeroForm')
+let emailProduct = document.getElementById('eMailForm');
+let datosFactura = document.getElementById('datosFactura')
+let datosFacturaTotal = document.getElementById('datosFacturaTotal')
+
+let btnPagar = document.getElementById('pagarCarro');
+
+let sumar = function(n1,n2){
+    return n1 + n2
+}
+
+//funcion constructora
+function darPorcentaje (a,b){
+    let descuento = a*b;
+    return descuento;
+}
 
 
 
+function aplicarPromo (a,b){
+    let totalAplicado = a-b;
+    return totalAplicado;
+}
 
+// factura;
 
-//descuento
-let descuentoPar = document.getElementById('descuento')
+if(btnPagar){
+    btnPagar.addEventListener('click', (e)=>{
+        e.preventDefault();
+        localStorage.setItem('user', userProduct.value);
+        localStorage.setItem('apellidos', lastNameProduct.value);
+        localStorage.setItem('celular', cellProduct.value);
+        localStorage.setItem('email', emailProduct.value);
+        let user = localStorage.getItem('user');
+        let apellidos = localStorage.getItem('apellidos');
+        let celular = localStorage.getItem('celular');
+        let email = localStorage.getItem('email');
 
-let promoPar = carritoCompras.length;
-console.log("hola "+promoPar);
+        let total= carritoCompras.reduce(sumar);
 
-while(promoPar > 1){
-    descuentoPar.innerHTML += '<p>20%</p>';
-    brake;
+        console.log(total);
+
+        let promoPar = carritoCompras.length;
+        let porciento;
+        let porciento2;
+
+        while(promoPar > 1){
+        porciento="20%"
+        porciento2=.2
+        break;
+        }
+
+        let totalMenosDescuento = aplicarPromo(total,darPorcentaje(total,porciento2));
+
+        let carroFact = document.getElementById('productosFact')
+
+        datosFactura.innerHTML = '<h1>Factura</h1>'+ '<p>Nombre: '+user+'</p><p>Apellidos: '+apellidos+'</p><p>Celular de contacto: '+celular+'</p><p>E-m@il: '+email;
+
+        // carroFact.innerHTML = carritoComprasDatos;
+        
+        datosFacturaTotal.innerHTML = '<p>Total: $'+total+'.00</p>';
+
+        if(porciento=="20%"){
+            datosFacturaTotal.innerHTML += '<p>Descuento: '+porciento+'</p><p>Total con descuento aplicado: '+totalMenosDescuento+'</p>'
+        }
+        
+        console.log(aplicarPromo(promoPar,darPorcentaje(porciento2*promoPar)));
+
+        
+        for (i=0; i<carritoComprasDatos.length; i++){
+            let momento = carritoComprasDatos[i];
+            carroFact.innerHTML += '<p>'+momento+'</p>';
+        }
+
+    });
 }
